4a06a1806a8f832b112d5015b513a8281a7e47d4
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2025-02-11 09:45:05 -0500

    [1910] [Time Beats: 100.00%] [Memory Beats: 90.02%] - LeetPush

--- 1910-Remove-All-Occurrences-of-a-Substring.py
+++ 1910-Remove-All-Occurrences-of-a-Substring.py
@@ -1,18 +1,30 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
-        stack = []
-        part_length = len(part)
+        while part in s:
+            part_start_at = s.index(part)
 
-        for char in s:
-            stack.append(char)
+            ## update the string by removing indexes of part
+            s = s[:part_start_at] + s[part_start_at + len(part) :]
 
-            if len(stack) >= part_length and self._check_match(
-                stack, part, part_length
-            ):
-                for _ in range(part_length):
-                    stack.pop()
+        return s
 
-        return "".join(stack)
+
+
+# class Solution:
+#     def removeOccurrences(self, s: str, part: str) -> str:
+        # stack = []
+        # part_length = len(part)
+
+        # for char in s:
+        #     stack.append(char)
+
+        #     if len(stack) >= part_length and self._check_match(
+        #         stack, part, part_length
+        #     ):
+        #         for _ in range(part_length):
+        #             stack.pop()
+
+        # return "".join(stack)
 
     def _check_match(self, stack: list, part: str, part_length: int) -> bool:
         return "".join(stack[-part_length:]) == part
