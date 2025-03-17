1e0d2e68fc42b93606ae5dd233d77f65e04e9e0c
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2025-03-17 06:51:48 -0700

    [2206] [Time Beats: 42.68%] [Memory Beats: 5.32%] - LeetPush

--- Divide-Array-Into-Equal-Pairs.py
+++ Divide-Array-Into-Equal-Pairs.py
@@ -1,17 +1,11 @@
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
-        count = defaultdict(int)
+        count = {}
         for num in nums:
-            if count[num]:
-                count[num] += 1
-                if count[num] % 2 == 0:
-                    del(count[num])
-            else:
-                count[num] = 1
+            if num not in count:
+                count[num] = 0
+            count[num] += 1
 
-        if count:
+        if len(list(filter(lambda k: k%2!=0, count.values()))):
             return False
-        else:
-            return True
-
-        
+        return True
