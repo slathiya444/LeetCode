7e01d5bf54bda1ca6615ab31fc7a445c31370765
slathiya444 <58988679+slathiya444@users.noreply.github.com>
2025-04-05 06:06:59 -0700

    [1863] [Time Beats: 76.73%] [Memory Beats: 41.01%] - LeetPush

--- Sum-of-All-Subset-XOR-Totals.py
+++ Sum-of-All-Subset-XOR-Totals.py
@@ -1,31 +1,11 @@
 class Solution:
     def subsetXORSum(self, nums):
 
-        def generate_subsets(nums, index, subset, subsets):
-            # Base case: index reached end of nums
-            # Add the current subset to subsets
-            if index == len(nums):
-                subsets.append(subset[:])
-                return
+        def dp(idx, total):
+            ## base case
+            if idx == len(nums):
+                return total
 
-            # Generate subsets with nums[i]
-            subset.append(nums[index])
-            generate_subsets(nums, index + 1, subset, subsets)
-            subset.pop()
-
-            # Generate subsets without nums[i]
-            generate_subsets(nums, index + 1, subset, subsets)
-
-        # Generate all of the subsets
-        subsets = []
-        generate_subsets(nums, 0, [], subsets)
-
-        # Compute the XOR total for each subset and add to the result
-        result = 0
-        for subset in subsets:
-            subset_XOR_total = 0
-            for num in subset:
-                subset_XOR_total ^= num
-            result += subset_XOR_total
-
-        return result
+            return dp(idx+1, total ^ nums[idx]) + dp(idx+1, total)
+            
+        return dp(0, 0)
